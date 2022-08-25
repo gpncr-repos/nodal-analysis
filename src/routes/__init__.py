3c79bec0d666bce37731c4fa58f07fd62a1bd264
Alex <32038216+AlexeyVodopyan@users.noreply.github.com>
2022-08-25 19:20:49 +0300

    feat(it-school): удалены файлы

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,4 +1,3 @@
-from src.calculations.nodal import calc_nodal
 from fastapi import APIRouter
 from src.models.models import NodalCalcRequest, NodalCalcResponse
 
@@ -7,5 +6,9 @@ main_router = APIRouter(prefix="/nodal", tags=["NodalAnalysis"])
 
 @main_router.post("/calc", response_model=NodalCalcResponse)
 async def my_profile(data: NodalCalcRequest):
-    result = NodalCalcResponse.parse_obj(calc_nodal(**data.dict()))
-    return result
+    """
+    Эндпоинт для выполнения Узлового Анализа
+    """
+    # Функция для выполнения узлового анализа
+    from src.calculations.nodal import calc_nodal
+    pass
